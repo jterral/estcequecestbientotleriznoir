d341dd2214d87094fdcc0ab012e10908c13f8448
jootl <jonathan.terral@outlook.com>
2022-06-14 11:37:35 +0200

    fix: custom domain

--- terraform/blackrice-domain.tf
+++ terraform/blackrice-domain.tf
@@ -1,20 +1,20 @@
 # blackrice-domain.tf
 
-data "azurerm_dns_zone" "static_web_dnszone" {
-  name                = "www.estcequecestbientotleriznoir.fr"
+data "azurerm_dns_zone" "static-web-dnszone" {
+  name                = "estcequecestbientotleriznoir.fr"
   resource_group_name = azurerm_resource_group.resource-group.name
 }
 
-resource "azurerm_dns_cname_record" "static_web_dnszone_record" {
-  name                = "static-web-domain_cname_record"
-  zone_name           = data.azurerm_dns_zone.static_web_dnszone.name
-  resource_group_name = data.azurerm_dns_zone.static_web_dnszone.resource_group_name
+resource "azurerm_dns_cname_record" "static-web-dnszone-record" {
+  name                = "www"
+  zone_name           = data.azurerm_dns_zone.static-web-dnszone.name
+  resource_group_name = data.azurerm_dns_zone.static-web-dnszone.resource_group_name
   ttl                 = 3600
   target_resource_id  = azurerm_cdn_endpoint.static-web-endpoint.id
 }
 
-resource "azurerm_cdn_endpoint_custom_domain" "static_web_dnszone_cdomain" {
-  name            = "static_web_dnszone_cdomain"
+resource "azurerm_cdn_endpoint_custom_domain" "static-web-dnszone-cdomain" {
+  name            = "blackrice-domain"
   cdn_endpoint_id = azurerm_cdn_endpoint.static-web-endpoint.id
-  host_name       = "${azurerm_dns_cname_record.static_web_dnszone_record.name}.${data.azurerm_dns_zone.static_web_dnszone.name}"
+  host_name       = "${azurerm_dns_cname_record.static-web-dnszone-record.name}.${data.azurerm_dns_zone.static-web-dnszone.name}"
 }
